1216020ec1fb90854525d523306772276ba778a4
Shahan Ahmad <90104740+httpshahan@users.noreply.github.com>
2023-10-01 15:08:23 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def data_process(data):
+    if data: print("Data exists!"); return data*2;  # Multiple statements in one line
+    else: 
+      return None
+
+def add(a,b): return a+b   # No spaces around the operator
+
+password = "12345"  # Hardcoded password
+
+print(data_process([]))
+print(add(5,3))
